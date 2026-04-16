70cdd7af2940255f484069723993ffd0eeb0bffd
ankityad971 <ankityad971@gmail.com>
2026-04-16 10:16:17 +0530

    Refactor zero removal logic in array

--- 35_remove0End.cpp
+++ 35_remove0End.cpp
@@ -5,9 +5,8 @@ int main() {
     int arr[] = {0, 1, 0, 3, 12};
     int n = 5;
 
-    int j = 0; // position for non-zero elements
+    int j = 0;
 
-    // Move non-zero elements forward
     for(int i = 0; i < n; i++) {
         if(arr[i] != 0) {
             arr[j] = arr[i];
@@ -15,13 +14,11 @@ int main() {
         }
     }
 
-    // Fill remaining positions with 0
     while(j < n) {
         arr[j] = 0;
         j++;
     }
 
-    // Print result
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
